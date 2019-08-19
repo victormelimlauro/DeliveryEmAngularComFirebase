994e16fa8e34b77e3816efbd7ec55b926cd151c8
OvidioGeronimo <51345446+OvidioGeronimo@users.noreply.github.com>
2019-08-19 19:20:00 -0300

    Delete firebase-config.ts

--- src/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseconfig = {
-  apiKey: "AIzaSyA4PQ4xLShnAVf9iiqHQwQsbUCm1jJJmCU",
-  authDomain: "delivery-9a001.firebaseapp.com",
-  databaseURL: "https://delivery-9a001.firebaseio.com",
-  projectId: "delivery-9a001",
-  storageBucket: "",
-  messagingSenderId: "501717088162",
-  appId: "1:501717088162:web:3f85b7cb0d8b69b9"
-}
