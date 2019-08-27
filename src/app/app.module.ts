f7b67175036a92ebf0e64f884616cf4ba1b8360c
VICTOR MELIM LAURO <victor.mlauro@senacsp.edu.br>
2019-08-26 22:22:55 -0300

    tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AngularFireStorageModule } from '@angular/fire/storage';
@@ -19,6 +20,9 @@ import { ListaCategoriasComponent } from './categorias/lista-categorias/lista-ca
 import { environment } from '../environments/environment';
 import { FormProdutosComponent } from './produtos/form-produtos/form-produtos.component';
 import { ListaProdutosComponent } from './produtos/lista-produtos/lista-produtos.component';
+import { CriarContaComponent } from './usuarios/criar-conta/criar-conta.component';
+import { LoginComponent } from './usuarios/login/login.component';
+import { EsqueciSenhaComponent } from './usuarios/esqueci-senha/esqueci-senha.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +35,10 @@ import { ListaProdutosComponent } from './produtos/lista-produtos/lista-produtos
     FormCategoriasComponent,
     ListaCategoriasComponent,
     FormProdutosComponent,
-    ListaProdutosComponent
+    ListaProdutosComponent,
+    CriarContaComponent,
+    LoginComponent,
+    EsqueciSenhaComponent
   ],
   imports: [
     BrowserModule,
@@ -40,6 +47,7 @@ import { ListaProdutosComponent } from './produtos/lista-produtos/lista-produtos
     FormsModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     AngularFireDatabaseModule,
     AngularFireStorageModule,
     ToastrModule.forRoot()
